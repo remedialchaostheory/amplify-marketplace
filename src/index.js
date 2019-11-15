7711137442bad0170418bc1355e069ee810ef561
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-15 11:22:35 -0800

    Import Amplify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Amplify from "aws-amplify";
+import aws_exports from "./aws-exports"
 import * as serviceWorker from "./serviceWorker";
 
 // Bring in default Element React theme
 import "element-theme-default";
 
+Amplify.configure(aws_exports);
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
