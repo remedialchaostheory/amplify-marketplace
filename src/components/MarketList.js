2502e88c903b0a43cdb0b9b515652681fbf25a6e
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-16 13:20:13 -0800

    Add subscription to newly created markets

--- src/components/MarketList.js
+++ src/components/MarketList.js
@@ -2,14 +2,26 @@ import React from "react";
 import { graphqlOperation } from 'aws-amplify';
 import { Connect } from 'aws-amplify-react';
 import { listMarkets } from "../graphql/queries";
+import { onCreateMarket } from "../graphql/subscriptions";
 import { Loading, Card, Icon, Tag } from "element-react";
 import { Link } from 'react-router-dom';
 import Error from "./Error";
 
 const MarketList = () => {
+  const onNewMarket = (prevQuery, newData) => {
+    let updatedQuery = { ...prevQuery };
+    const updatedMarketList = [
+        newData.onCreateMarket,
+        ...prevQuery.listMarkets.items
+    ];
+    updatedQuery.listMarkets.items = updatedMarketList;
+    return updatedQuery;
+  };
   return (
       <Connect
         query={graphqlOperation(listMarkets)}
+        subscription={graphqlOperation(onCreateMarket)}
+        onSubscriptionMsg={onNewMarket}
       >
         {({data, loading, errors }) => {
           if (errors.length > 0) return <Error errors={errors} />;
@@ -18,7 +30,7 @@ const MarketList = () => {
           return (
               <>
                 <h2 className="header">
-                  <img src="https://icon.now.sh/store_mall_director/527FFF"
+                  <img src="https://icon.now.sh/store_mall_directory/527FFF"
                        alt="Store Icon"
                        className="large-icon"
                   />
