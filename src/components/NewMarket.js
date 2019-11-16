60855255a655f56dae1fc7ab72c27d9a7d8527f9
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-16 10:32:47 -0800

    Add optional tags to new market creation

--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -8,6 +8,9 @@ import { UserContext } from "../App";
 class NewMarket extends React.Component {
   state = {
     name: "",
+    tags: ["Arts", "Technology", "Learning", "Entertainment", "Crafts", "Music"],
+    selectedTags: [],
+    options: [],
     addMarketDialog: false,
   };
 
@@ -18,11 +21,13 @@ class NewMarket extends React.Component {
       this.setState({addMarketDialog: false});
       const input = {
         name: this.state.name,
+        tags: this.state.selectedTags,
         owner: user.attributes.email,
       };
       const resp = await API.graphql(graphqlOperation(createMarket, {input}));
+      console.log({ resp });
       console.info(`Created market: id ${resp.data.createMarket.id}`);
-      this.setState({ name: "" });
+      this.setState({ name: "", selectedTags: [] });
     } catch (err) {
       console.error('Error adding new market', err);
       Notification.error({
@@ -33,6 +38,12 @@ class NewMarket extends React.Component {
 
   };
 
+  handleFilterTags = query => {
+    const options = this.state.tags
+        .map(tag => ({ value: tag, label: tag }))
+        .filter(tag => tag.label.toLowerCase().includes(query.toLowerCase()));
+    this.setState({ options })
+  };
   render() {
     return (
         <UserContext.Consumer>
@@ -67,6 +78,24 @@ class NewMarket extends React.Component {
                       onChange={name => this.setState({ name })}
                       value={this.state.name}
                   />
+                </Form.Item>
+                <Form.Item label="Add Tags">
+                  <Select
+                      multiple={true}
+                      filterable={true}
+                      placeholder="Market Tags"
+                      onChange={selectedTags => this.setState({ selectedTags })}
+                      remoteMethod={this.handleFilterTags}
+                      remote={true}
+                  >
+                  {this.state.options.map(option => (
+                       <Select.Option
+                           key={option.value}
+                           label={option.label}
+                           value={option.value}
+                       />
+                  ))}
+                  </Select>
                 </Form.Item>
               </Form>
             </Dialog.Body>
