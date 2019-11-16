52dbe9367d01b69451f764a0cdd42914cd08127b
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-15 22:46:38 -0800

    Add dialog box for creating a new market

--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -1,10 +1,17 @@
 import React from "react";
 // prettier-ignore
+import { API, graphqlOperation } from "aws-amplify";
+import { createMarket } from "../graphql/mutations";
 import { Form, Button, Dialog, Input, Select, Notification } from 'element-react'
 
 class NewMarket extends React.Component {
-  state = {};
+  state = {
+    name: "",
+    addMarketDialog: false,
+  };
 
+  handleAddMarket = async () => {
+  };
   render() {
     return (
         <React.Fragment>
@@ -15,10 +22,43 @@ class NewMarket extends React.Component {
                   type="text"
                   icon="edit"
                   className="market-title-button"
+                  onClick={() => this.setState({ addMarketDialog: true })}
               />
 
             </h1>
           </div>
+
+          <Dialog
+              title="Create New Market"
+              visible={this.state.addMarketDialog}
+              onCancel={() => this.setState({ addMarketDialog: false })}
+              size="large"
+              customClass="dialog"
+          >
+            <Dialog.Body>
+              <Form labelPosition="top">
+                <Form.Item label="Add Market Name">
+                  <Input
+                      placeholder="Market Name"
+                      trim={true}
+                      onChange={name => this.setState({ name })}
+                  />
+                </Form.Item>
+              </Form>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Button onClick={() => this.setState({ addMarketDialog: false })}>
+                Cancel
+              </Button>
+              <Button
+                  type="primary"
+                  disabled={!this.state.name}
+                  onClick={this.handleAddMarket}
+              >
+                Add
+              </Button>
+            </Dialog.Footer>
+          </Dialog>
         </React.Fragment>
     )
   }
