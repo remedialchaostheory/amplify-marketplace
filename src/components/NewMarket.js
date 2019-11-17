3847fd2732b32d8319b387f1ed11a0449147f6b8
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-16 18:22:09 -0800

    Add search of markets

--- src/components/NewMarket.js
+++ src/components/NewMarket.js
@@ -60,6 +60,28 @@ class NewMarket extends React.Component {
               />
 
             </h1>
+
+            <Form inline={true} onSubmit={this.props.handleSearch}>
+              <Form.Item>
+                <Input
+                    placeholder="Search Markets..."
+                    value={this.props.searchTerm}
+                    icon="circle-cross"
+                    onIconClick={this.props.handleClearSearch}
+                    onChange={this.props.handleSearchChange}
+                />
+              </Form.Item>
+              <Form.Item>
+                <Button
+                    type="info"
+                    icon="search"
+                    onClick={this.props.handleSearch}
+                    loading={this.props.isSearching}
+                >
+                  Search
+                </Button>
+              </Form.Item>
+            </Form>
           </div>
 
           <Dialog
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,12 +3,30 @@ import NewMarket from "../components/NewMarket";
 import MarketList from "../components/MarketList";
 
 class HomePage extends React.Component {
-  state = {};
+  state = {
+    searchTerm: "",
+    searchResults: [],
+    isSearching: false,
+  };
 
+  handleSearchChange = searchTerm => this.setState({ searchTerm });
+
+  handleClearSearch = () => this.setState( { searchTerm: "", searchResults: [] });
+
+  handleSearch = event => {
+    event.preventDefault();
+    console.log('this.state.searchTerm ->', this.state.searchTerm);
+  };
   render() {
     return (
         <React.Fragment>
-          <NewMarket />
+          <NewMarket
+              searchTerm={this.state.searchTerm}
+              isSearching={this.state.isSearching}
+              handleSearchChange={this.handleSearchChange}
+              handleClearSearch={this.handleClearSearch}
+              handleSearch={this.handleSearch}
+          />
           <MarketList />
         </React.Fragment>
     )
