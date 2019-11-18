f35e7b7c380194b12ade8166313364d2c7b9ad55
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-18 13:03:25 -0800

    Add display of product items

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,56 @@
 import React from "react";
+import { S3Image } from "aws-amplify-react";
 // prettier-ignore
-// import { Notification, Popover, Button, Dialog, Card, Form, Input, Radio } from "element-react";
+import { Notification, Popover, Button, Dialog, Card, Form, Input, Radio } from "element-react";
+import { convertCentsToDollars } from "../utils/index";
+import { UserContext } from "../App";
+import PayButton from "./PayButton";
 
 class Product extends React.Component {
   state = {};
-  
+
   render() {
-    return <div>Product</div>
+    const { product } = this.props;
+
+    return (
+      <UserContext.Consumer>
+        {({ user }) => {
+          const isProductOwner = user && user.attributes.sub === product.owner;
+          console.log("user ->", user);
+          return (
+            <div className="card-container">
+              <Card bodyStyle={{ padding: 0, minWidth: "200px" }}>
+                <S3Image
+                  imgKey={product.file.key}
+                  theme={{
+                    photoImg: { maxWidth: "100%", maxHeight: "100%" },
+                  }}
+                />
+                <div className="card-b">
+                  <h3 className="m-0">{product.description}</h3>
+                  <div className="items-center">
+                    <img
+                      src={`https://icon.now.sh/${
+                        product.shipped ? "markunread_mailbox" : "mail"
+                      }`}
+                      alt="Shipping Icon"
+                      className="icon"
+                    />
+                    {product.shipped ? "Shipped" : "Emailed"}
+                  </div>
+                  <div className="text-right">
+                    <span className="mx-1">
+                      ${convertCentsToDollars(product.price)}
+                    </span>
+                    {!isProductOwner && <PayButton />}
+                  </div>
+                </div>
+              </Card>
+            </div>
+          );
+        }}
+      </UserContext.Consumer>
+    );
   }
 }
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,1 +1,3 @@
 export const convertDollarsToCents = price => (price * 100).toFixed(0);
+
+export const convertCentsToDollars = price => (price / 100).toFixed(2);
