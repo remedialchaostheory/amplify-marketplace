c1248f660bb2652cf6296713753444a154af8b36
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-19 11:17:08 -0800

    Auto format with Prettier

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,37 +3,41 @@ import { Menu as Nav, Icon, Button } from "element-react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = ({ user, handleSignOut }) => (
-    <Nav mode="horizontal" theme="dark" defaultActive="1">
-        <div className="nav-container">
-          {/*Title*/}
-          <Nav.Item index="1">
-            <NavLink to="/" className="nav-link">
-              <span className="app-title">
-                <img src="https://icon.now.sh/account_balance/f90"
-                     alt="App Icon"
-                     className="app-icon" />
-                Marketplace
-              </span>
-            </NavLink>
-          </Nav.Item>
+  <Nav mode="horizontal" theme="dark" defaultActive="1">
+    <div className="nav-container">
+      {/*Title*/}
+      <Nav.Item index="1">
+        <NavLink to="/" className="nav-link">
+          <span className="app-title">
+            <img
+              src="https://icon.now.sh/widgets/f90"
+              alt="App Icon"
+              className="app-icon"
+            />
+            Marketplace
+          </span>
+        </NavLink>
+      </Nav.Item>
 
-          {/* Navbar Items */}
-          <div className="nav-items">
-            <Nav.Item index="2">
-              <span className="app-user">Hi, {user.attributes.email}</span>
-            </Nav.Item>
-            <Nav.Item index="3">
-              <NavLink to="/profile" className="nav-link">
-                <Icon name="setting" />
-                Profile
-              </NavLink>
-            </Nav.Item>
-            <Nav.Item index="4">
-              <Button type="warning" onClick={handleSignOut}>Sign Out</Button>
-            </Nav.Item>
-          </div>
-        </div>
-    </Nav>
-)
+      {/* Navbar Items */}
+      <div className="nav-items">
+        <Nav.Item index="2">
+          <span className="app-user">Hi, {user.attributes.email}</span>
+        </Nav.Item>
+        <Nav.Item index="3">
+          <NavLink to="/profile" className="nav-link">
+            <Icon name="setting" />
+            Profile
+          </NavLink>
+        </Nav.Item>
+        <Nav.Item index="4">
+          <Button type="warning" onClick={handleSignOut}>
+            Sign Out
+          </Button>
+        </Nav.Item>
+      </div>
+    </div>
+  </Nav>
+);
 
 export default Navbar;
