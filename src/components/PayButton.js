b2d25f04807487e40195f29e7f9181043478eea7
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-11-22 11:04:21 -0800

    Add pay button via Stripe

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,8 +1,19 @@
 import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 // import { Notification, Message } from "element-react";
 
+const stripeConfig = {
+  currency: "USD",
+  publishableAPIKey: "pk_test_Sh2VtPNSbsjWAzzu6PAOpg3H00untteNHo",
+};
+
 const PayButton = () => {
-  return <div>PayButton</div>;
+  return (
+    <StripeCheckout
+      currency={stripeConfig.currency}
+      stripeKey={stripeConfig.publishableAPIKey}
+    />
+  );
 };
 
 export default PayButton;
